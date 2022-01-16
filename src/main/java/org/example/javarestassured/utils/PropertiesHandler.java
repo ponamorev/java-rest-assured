423806ae27c0c2a048661a331c66bc875ad3cfdb
Vyacheslav Ponamorev <vy.ponamorev@andersenlab.com>
2022-01-16 19:18:59 +0300

    Added base specification and properties

---
+++ src/main/java/org/example/javarestassured/utils/PropertiesHandler.java
@@ -0,0 +1,23 @@
+package org.example.javarestassured.utils;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Objects;
+import java.util.Properties;
+
+public class PropertiesHandler {
+    private static Properties properties;
+
+    public static Properties getProperties() {
+        if (Objects.isNull(properties)) {
+            try (FileInputStream inputStream = new FileInputStream("/src/main/resources/app.properties")) {
+                properties = new Properties();
+                properties.load(inputStream);
+                return properties;
+            } catch (IOException e) {
+                System.err.println("There was a problem with properties loading");
+            }
+        }
+        return properties;
+    }
+}
